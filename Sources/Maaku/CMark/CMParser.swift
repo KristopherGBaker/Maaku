2936d58baea3a71088f89e218f292ba4a1bfd4d0
Kristopher Baker <kristopher.baker@smartnews.com>
2019-06-19 15:12:46 +0900

    Fix a few line length violations

--- Sources/Maaku/CMark/CMParser.swift
+++ Sources/Maaku/CMark/CMParser.swift
@@ -507,15 +507,23 @@ public class CMParser {
             }
         case .link:
             if eventType == .enter {
-                try delegate?.parser(parser: self, didStartLinkWithDestination: node.linkDestination, title: node.linkTitle)
+                try delegate?.parser(parser: self,
+                                     didStartLinkWithDestination: node.linkDestination,
+                                     title: node.linkTitle)
             } else {
-                try delegate?.parser(parser: self, didEndLinkWithDestination: node.linkDestination, title: node.linkTitle)
+                try delegate?.parser(parser: self,
+                                     didEndLinkWithDestination: node.linkDestination,
+                                     title: node.linkTitle)
             }
         case .image:
             if eventType == .enter {
-                try delegate?.parser(parser: self, didStartImageWithDestination: node.linkDestination, title: node.linkTitle)
+                try delegate?.parser(parser: self,
+                                     didStartImageWithDestination: node.linkDestination,
+                                     title: node.linkTitle)
             } else {
-                try delegate?.parser(parser: self, didEndImageWithDestination: node.linkDestination, title: node.linkTitle)
+                try delegate?.parser(parser: self,
+                                     didEndImageWithDestination: node.linkDestination,
+                                     title: node.linkTitle)
             }
         case .htmlBlock:
             try delegate?.parser(parser: self, foundHtml: node.stringValue ?? "")
